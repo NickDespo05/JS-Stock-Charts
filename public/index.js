b6d9d245771fcece11fc187481098b261d876c60
Nick D'Esposito <Nicholas D'Esposito>
2022-05-28 09:47:46 -0400

    Not Done Yet

--- public/index.js
+++ public/index.js
@@ -7,9 +7,14 @@ async function main() {
     //     "#average-price-chart"
     // );
     //prettier-ignore
-    let stocks = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT/DIS,BNTX:Huobi,TRP:TSX&interval=1min&apikey=27d06d6c78614f12a3ca675fae22e9e5")
+    let stocks = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX,TRP:TSX&interval=1min&apikey=27d06d6c78614f12a3ca675fae22e9e5", {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 
     let response = await stocks.json();
+    // const stockLists = [GME, MSFT, DIS, BNTX];
     console.log(response);
 }
 
