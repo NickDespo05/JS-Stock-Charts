1648ed5021b7d17b5697d60d165d0021b11d65b6
Nick D'Esposito <Nicholas D'Esposito>
2022-05-27 22:39:04 -0400

    Work on the Fetch not being defined

--- public/index.js
+++ public/index.js
@@ -1,9 +1,16 @@
 async function main() {
+    // const timeChartCanvas = document.querySelector("#time-chart");
+    // const highestPriceChartCanvas = document.querySelector(
+    //     "#highest-price-chart"
+    // );
+    // const averagePriceChartCanvas = document.querySelector(
+    //     "#average-price-chart"
+    // );
+    //prettier-ignore
+    let stocks = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT/DIS,BNTX:Huobi,TRP:TSX&interval=1min&apikey=27d06d6c78614f12a3ca675fae22e9e5")
 
-    const timeChartCanvas = document.querySelector('#time-chart');
-    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
-    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
-
+    let response = await stocks.json();
+    console.log(response);
 }
 
-main()
+main();
